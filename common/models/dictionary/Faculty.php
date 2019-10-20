1dc6040dc81c11c2f0095e915bbf32be7988cc54
Mark <markpdd13@list.ru>
2019-10-20 12:22:32 +0300

    Dictionary Faculty  #1

---
+++ common/models/dictionary/Faculty.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace  common\models\dictionary;
+
+class Faculty extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'dict_faculty';
+    }
+
+    public static function create (string $fullName) : self
+    {
+        $faculty =  new static();
+        $faculty->full_name = $fullName;
+        return $faculty;
+    }
+
+    public function edit(string $fullName) :void
+    {
+        $this->full_name = $fullName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+             'id' => 'ИL',
+            'full_name' => 'Полное название',
+        ];
+    }
+
+    public static function labels(): array
+    {
+        $faculty =  new static();
+        return $faculty->attributeLabels();
+    }
+}
