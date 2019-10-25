da82bec1faf46a0a53705a7374803c8e49a1a32e
Mark <markpdd13@list.ru>
2019-10-26 01:55:09 +0300

    #14 Olympic

---
+++ dictionary/helpers/DictChairmansHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace helpers;
+
+
+use dictionary\models\DictChairmans;
+use yii\helpers\ArrayHelper;
+
+class DictChairmansHelper
+{
+    public static function chairmansFullNameList(): array
+    {
+        return ArrayHelper::map(DictChairmans::find()->asArray()->all(), "id", function (array  $model){
+            return $model['last_name']." ".$model['first_name']." ".$model['patronymic'];
+        });
+    }
+
+    public static function chairmansFullNameOne($key): string
+    {
+        return ArrayHelper::getValue(self::chairmansFullNameList(), $key);
+    }
+
+}
