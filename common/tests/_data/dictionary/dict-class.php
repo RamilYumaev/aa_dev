9adc53c6f0904512624a17907efe9fefae642cbb
Ramil <ramilka06@inbox.ru>
2019-10-29 18:06:24 +0300

    add broken test dictClass

---
+++ common/tests/_data/dictionary/dict-class.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    ['name'=> 1,
+        'type'=> \dictionary\helpers\DictClassHelper::SCHOOL],
+    ['name'=> 2,
+        'type'=> \dictionary\helpers\DictClassHelper::SCHOOL],
+    ['name'=> 3,
+        'type'=> \dictionary\helpers\DictClassHelper::SCHOOL],
+    ['name'=> 1,
+        'type'=> \dictionary\helpers\DictClassHelper::COLLEDGE],
+];
