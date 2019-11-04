a60332c406ad82fbe115194819ee52f955f31698
Ramil <ramilka06@inbox.ru>
2019-11-04 18:03:46 +0300

    add fixture data

--- common/tests/_data/dictionary/discipline-competitive-group.php
+++ common/tests/_data/dictionary/discipline-competitive-group.php
@@ -1,1 +1,8 @@
 <?php
+return [
+    [
+        'discipline_id' => '1',
+        'competitive_group_id' => '1',
+        'priority' => '3',
+    ],
+];
--- common/tests/_data/dictionary/templates.php
+++ common/tests/_data/dictionary/templates.php
@@ -1,1 +1,172 @@
 <?php
+return [
+    [
+        'name'=> 'Регламент мероприятия с заочным и очным турами (старый)',
+        'type_id'=> '1',
+        'text'=> '<p dir="ltr" style="text-align:right">&nbsp;</p>
+
+<p dir="ltr" style="text-align:right"><strong>&laquo;Утверждаю&raquo;</strong></p>
+
+<p dir="ltr" style="text-align:right"><span style="background-color:transparent; color:rgb(0, 0, 0); font-family:times new roman; font-size:13pt">Председатель оргкомитета</span></p>
+
+<p dir="ltr" style="text-align:right"><span style="background-color:transparent; color:rgb(0, 0, 0); font-family:times new roman; font-size:13pt">______________________________{Фамилия И.О. председателя}</span></p>
+
+<p>&nbsp;</p>
+
+<p dir="ltr" style="text-align:center"><strong>Регламент проведения</strong></p>
+
+<p dir="ltr" style="text-align:center"><strong>{Название мероприятия в родительном падеже}</strong></p>
+
+<p>&nbsp;</p>
+
+<ol>
+	<li dir="ltr">
+	<p dir="ltr"><strong>ОБЩЕЕ ПОЛОЖЕНИЕ</strong></p>
+	</li>
+</ol>
+
+<ol>
+	<li dir="ltr">
+	<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Настоящий Регламент разработан в соответствии с Федеральным законом от 29 декабря 2012 г. №273-ФЗ &laquo;Об образовании в Российской Федерации&raquo;, Приказом Министерства образования и науки Российской Федерации от 14 октября 2015 г. № 1147 &laquo;Об утверждении Порядка приема на обучение по образовательным программам высшего образования &ndash; программам бакалавриата, программам магистратуры&raquo;, Правилами приема на обучение по образовательным программам высшего образования &ndash; программам бакалавриата, программам магистратуры, программам подготовки научно-педагогических кадров в аспирантуре в МПГУ, Положением об олимпиадах и иных интеллектуальных и творческих конкурсах, не используемых для получения особых прав и (или) преимуществ при поступлении на обучение, проводимых МПГУ</span></p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Настоящий Регламент определяет правила проведения {Название мероприятия в родительном падеже} (далее &ndash; Мероприятие).</span></p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Сайтом Мероприятия является&nbsp;</span><a href="https://sdo.mpgu.org" style="text-decoration-line: none;"><span style="background-color:transparent; color:rgb(0, 0, 255); font-size:13pt">https://sdo.mpgu.org</span></a></p>
+	</li>
+</ol>
+
+<p dir="ltr" style="text-align:justify">&nbsp;</p>
+
+<ol start="2">
+	<li dir="ltr">
+	<p dir="ltr"><span style="background-color:transparent; font-size:13pt">УСЛОВИЯ РЕГИСТРАЦИИ, УЧАСТИЯ И ПРОВЕДЕНИЯ МЕРОПРИЯТИЯ</span></p>
+
+	<ol>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Участие в Мероприятие возможно только при предварительной регистрации на сайте&nbsp;</span><a href="https://sdo.mpgu.org" style="text-decoration-line: none;"><span style="background-color:transparent; color:rgb(0, 0, 0); font-size:13pt">https://sdo.mpgu.org</span></a><span style="background-color:transparent; font-size:13pt">&nbsp;.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">К участию в Мероприятие допускаются {классы/курсы участников}.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Регистрация открывается {дата и время начала регистрации} по московскому времени и заканчивается&nbsp;{дата и время завершения регистрации} по московскому времени.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Каждый участник регистрируется только один раз, повторное участие в Мероприятие в один год невозможно.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Мероприятие проходит в 2 тура &ndash; заочный и очный.</span></p>
+		</li>
+	</ol>
+	</li>
+</ol>
+
+<p>&nbsp;</p>
+
+<ol start="3">
+	<li dir="ltr">
+	<p dir="ltr"><strong>ПРОВЕДЕНИЕ ЗАОЧНОГО (ДИСТАНЦИОННОГО) ТУРА</strong></p>
+
+	<ol>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">К участию в заочном (дистанционном) туре допускаются зарегистрированные в период, указанный в подпункте 2.3. настоящего регламента пользователи.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Заочный (дистанционный) тур проводится в те же сроки, что и период регистрации участников на Мероприятие.&nbsp;</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">{пункт3}.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">В случае, если участник не успел завершить выполнение всех заданий в установленный срок &ndash; фиксируется результат, установленный на момент истечения времени.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">К участию в следующем туре допускаются участники, набравшие наивысший балл.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Решение о количестве участников очного тура принимается оргкомитетом Мероприятия исходя из общего количества участников заочного (дистанционного) тура.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Требования к выполнению заданий заочного (дистанционного) тура указаны в приложении №1 настоящего регламента.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Критерии оценивания заданий заочного (дистанционного) тура указаны в приложении №2 настоящего регламента.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Работы оцениваются по 100-балльной шкале.</span></p>
+		</li>
+	</ol>
+	</li>
+</ol>
+
+<p>&nbsp;</p>
+
+<ol start="4">
+	<li dir="ltr">
+	<p dir="ltr"><span style="background-color:transparent; font-size:13pt">ПРОВЕДЕНИЕ ОЧНОГО ТУРА</span></p>
+
+	<ol>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">К участию в очном туре допускаются только зарегистрировавшиеся участники, успешно прошедшие заочный (дистанционный) тур.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Дата и время проведения очного тура: {дата и время проведения очного тура}</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Место проведения очного тура: {адрес проведения очного тура}</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">В день проведения Мероприятия участник предоставляет организаторам документа, удостоверяющего личность или его копию.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">В случае отсутствия требуемых документов оргкомитет вправе отказать в участие в очном туре.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Время выполнения заданий составляет {продолжительность выполнения заданий очного тура в минутах}.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Требования к выполнению заданий очного тура указаны в приложении №3 настоящего регламента.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Критерии оценивания заданий очного тура указаны в приложении №4 настоящего регламента.</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Работы очного тура оцениваются по 100-балльной шкале.</span></p>
+		</li>
+	</ol>
+	</li>
+</ol>
+
+<p>&nbsp;</p>
+
+<ol start="5">
+	<li dir="ltr">
+	<p dir="ltr"><strong>РЕЗУЛЬТАТЫ МЕРОПРИЯТИЯ</strong></p>
+
+	<ol>
+		<li dir="ltr">
+		<p dir="ltr"><span style="background-color:transparent; font-size:13pt">По результатам мероприятия победители и призеры получают электронные версии дипломов или сертификатов в личном кабинете на сайте https://sdo.mpgu.org .</span></p>
+		</li>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">При поступлении в МПГУ на направлени(е/я) {выбранные конкурсные группы} победителям Мероприятия засчитываются дополнительные 3 балла, призерам II степени &ndash; 2 балла, призерам III степени &nbsp;&ndash; 1 балл в качестве индивидуальных достижений.</span></p>
+		</li>
+	</ol>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr"><strong>ПОКАЗ РАБОТ И АПЕЛЛЯЦИЯ</strong></p>
+
+	<ol>
+		<li dir="ltr">
+		<p dir="ltr" style="text-align:justify"><span style="background-color:transparent; font-size:13pt">Показ работ и апелляция для очного тура настоящего Мероприятия {показ работ и апелляция}.</span></p>
+		</li>
+	</ol>
+	</li>
+</ol>
+
+<p>{текст аппеляции}</p>
+',
+        'name_for_user'=> 'Регламент',
+    ],
+];
