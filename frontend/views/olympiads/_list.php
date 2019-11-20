0f59ee5fc5e88de08cf17ea30db47ffcb853fc23
Mark <markpdd13@list.ru>
2019-11-20 13:02:37 +0300

    Select2 Olympic School

--- frontend/views/olympiads/_list.php
+++ frontend/views/olympiads/_list.php
@@ -1,9 +1,16 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-
+<div class="container">
 <?=\yii\widgets\ListView::widget([
     'dataProvider' => $dataProvider,
     'layout' => "{items}\n{pager}",
+    'beforeItem' => function($model , $key , $index , $widget) {
+          return  $index%3 == 0 ? '<div class="row">':"";
+        },
+    'afterItem' => function($model , $key , $index , $widget) {
+          return  $index%3 == 2 ? '</div>': "";
+        } ,
     'itemView' => '_olympic',
 ]) ?>
+</div>
