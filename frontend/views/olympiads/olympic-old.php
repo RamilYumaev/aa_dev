0dc285ec628ec3a1e0b2f2c377834273b89b254e
Ramil <ramilka06@inbox.ru>
2019-11-21 20:11:46 +0300

    fix

--- frontend/views/olympiads/olympic-old.php
+++ frontend/views/olympiads/olympic-old.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use frontend\widgets\olympictemplates\OlympicTemplatesWidget;
 use frontend\widgets\olympic\OlympicResultWidget;
@@ -7,14 +8,19 @@ use frontend\widgets\olympic\OlympicResultWidget;
 /* @var $olympic \olympic\models\OlimpicList */
 
 $url = \yii\helpers\Url::to(['/olympiads']);
-$this->title = $olympic->name. " - ". $olympic->year;
+$this->title = $olympic->name . " - " . $olympic->year;
+$this->params['breadcrumbs'][] = ['label' => 'Олимпиады и конкурсы', 'url' => $url];
+$this->params['breadcrumbs'][] = ['label' => \olympic\helpers\OlympicHelper::olimpicName($olympic->olimpic_id),
+    'url' => ['registration-on-olympiads', 'id' => $olympic->olimpic_id]];
+$this->params['breadcrumbs'][] = $olympic->year;
 ?>
-<div class="container-fluid">
+<div class="container-fluid olimp bg<?= rand(1, 3) ?>">
     <p align="right"><?= $olympic->eduLevelString ?></p>
     <h1 align="center"><?= Html::encode($this->title) ?></h1>
     <div class="row">
         <div class="col-md-4"><p align="center"><?= $olympic->facultyNameString ?></p></div>
-        <div class="col-md-4"><p align="center"><?= $olympic->numberOftoursNameString?> <br /> <?= $olympic->onlyMpguStudentsString ?></p></div>
+        <div class="col-md-4"><p align="center"><?= $olympic->numberOftoursNameString ?>
+                <br/> <?= $olympic->onlyMpguStudentsString ?></p></div>
         <div class="col-md-4"><p align="center"><?= $olympic->formOfPassageString ?></p></div>
     </div>
 </div>
