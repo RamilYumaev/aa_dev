c1ef8d3f0198457d04083e36bc3d2db7d145cd58
Ramil <ramilka06@inbox.ru>
2019-11-28 11:14:52 +0300

    #2

--- common/fixtures/olympic/OlympicListFixture.php
+++ common/fixtures/olympic/OlympicListFixture.php
@@ -4,13 +4,14 @@ namespace common\fixtures\olympic;
 
 use common\fixtures\dictionary\DictChairmansFixture;
 use common\fixtures\dictionary\DictClassFixture;
+use common\fixtures\dictionary\DictCompetitiveGroupFixture;
 use olympic\models\OlimpicList;
 use yii\test\ActiveFixture;
 use common\fixtures\dictionary\FacultyFixture;
-use common\fixtures\dictionary\DictCompetitiveGroupFixture;
 
 class OlympicListFixture extends ActiveFixture
 {
     public $modelClass = OlimpicList::class;
-    public $depends = [DictChairmansFixture::class, OlympicFixture::class, FacultyFixture::class, DictCompetitiveGroupFixture::class, DictClassFixture::class];
+    public $depends = [DictChairmansFixture::class, OlympicFixture::class, FacultyFixture::class,
+        DictClassFixture::class, DictCompetitiveGroupFixture::class];
 }
