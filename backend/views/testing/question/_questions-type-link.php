64550b2031bbaf112742ca4e5db63a678467ab50
Ramil <ramilka06@inbox.ru>
2019-11-28 13:30:22 +0300

    typo fix

--- backend/views/testing/question/_questions-type-link.php
+++ backend/views/testing/question/_questions-type-link.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
     <div class="col-md-12">
         <?= Html::a('Выбрать вариант(ы)', ['/testing/question/types/type-select'], [ 'class'=>'btn btn-success']); ?>
         <?= Html::a('Выбрать вариант', ['/testing/question/types/type-select-one'], [ 'class'=>'btn btn-success']); ?>
-        <?= Html::a('Сопаставить', ['/testing/question/types/type-matching'], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Сопоставить', ['/testing/question/types/type-matching'], [ 'class'=>'btn btn-success']); ?>
         <?= Html::a('Краткий ответ', ['/testing/question/types/type-answer-short'], [ 'class'=>'btn btn-success']); ?>
         <?= Html::a('Развернутый ответ', ['/testing/question/types/type-answer-detailed'], [ 'class'=>'btn btn-success']); ?>
         <?= Html::a('Загрузка файла', ['/testing/question/types/type-file'], [ 'class'=>'btn btn-success']); ?>
