389048ac4144e02ffbec2e2277356218479b23cc
Ramil <ramilka06@inbox.ru>
2019-12-07 17:05:49 +0300

    change table

--- console/migrations/m191207_135028_add__manager_id_into_olimpic_list.php
+++ console/migrations/m191207_135028_add__manager_id_into_olimpic_list.php
@@ -12,7 +12,7 @@ class m191207_135028_add__manager_id_into_olimpic_list extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('{{olimpic_list}}', 'manager_id', $this->integer()->defaultValue(NULL)->comment("отвественный за олимпиаду"));
+        $this->addColumn('{{olimpic}}', 'manager_id', $this->integer()->defaultValue(NULL)->comment("отвественный за олимпиаду"));
 
     }
 
@@ -21,24 +21,9 @@ class m191207_135028_add__manager_id_into_olimpic_list extends Migration
      */
     public function safeDown()
     {
-        $this->dropColumn('{{olimpic_list}}', 'manager_id');
+        $this->dropColumn('{{olimpic}}', 'manager_id');
         echo "m191207_135028_add__manager_id_into_olimpic_list cannot be reverted.\n";
 
         return false;
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m191207_135028_add__manager_id_into_olimpic_list cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
