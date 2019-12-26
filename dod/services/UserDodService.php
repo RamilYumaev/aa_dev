ea74c0607540989aedc3d7cc7c911eca766e976e
Mark <markpdd13@list.ru>
2019-12-26 13:34:49 +0300

    Dod  add Profile

--- dod/services/UserDodService.php
+++ dod/services/UserDodService.php
@@ -1,24 +1,27 @@
 <?php
 namespace dod\services;
 
-
 use dod\models\UserDod;
 use dod\repositories\DateDodRepository;
 use dod\repositories\UserDodRepository;
+use olympic\repositories\auth\ProfileRepository;
 
 class UserDodService
 {
     private $dateDodRepository;
     private $repository;
+    private $profileRepository;
 
-    function __construct(UserDodRepository $repository, DateDodRepository $dateDodRepository)
+    function __construct(UserDodRepository $repository, ProfileRepository $profileRepository, DateDodRepository $dateDodRepository)
     {
         $this->repository = $repository;
         $this->dateDodRepository = $dateDodRepository;
+        $this->profileRepository= $profileRepository;
     }
 
     public function add($dod_id, $user_id) {
         $dod = $this->dateDodRepository->get($dod_id);
+        $profile = $this->isAddProfile();
         $userDod = UserDod::create($dod->id, $user_id);
         $this->repository->save($userDod);
     }
@@ -26,6 +29,16 @@ class UserDodService
     public function remove($dod_id, $user_id) {
         $userDod = $this->repository->get($dod_id, $user_id);
         $this->repository->remove($userDod);
+    }
+
+    private function isAddProfile () {
+        $profile = $this->profileRepository->getUserId();
+        if(is_null($profile)) {
+            throw new \DomainException('Необходимо заполнить профиль');
+        } else if ($profile->isNullProfile()) {
+            throw new \DomainException('Необходимо заполнить профиль');
+        }
+        return $profile;
     }
 
 }
