115eac57ac775f3b5e3ad4f3a809ef03ba7eb922
Ramil <ramilka06@inbox.ru>
2020-01-12 20:20:33 +0300

    fix message1

--- frontend/views/schools/index.php
+++ frontend/views/schools/index.php
@@ -19,7 +19,7 @@ $isOlympicUser = Yii::$app->user->identity->isUserOlympic();
 <div class="container">
     <?php if ($isOlympicUser) : ?>
         <?= Yii::$app->session->setFlash('warning', 'Вы не можете добавлять/редактировать учебные организации, 
-        так как Вы записаны на одну из олимпиад '. \common\helpers\EduYearHelper::eduYear()). " учебного года" ?>
+        так как записаны на одну из олимпиад '. \common\helpers\EduYearHelper::eduYear()). ' учебного года' ?>
     <?php endif; ?>
 <div class="row">
     <div class="col-md-7">
