a9d4b855a4aaefffee11f78de1ac3900c52137dd
Ramil <ramilka06@inbox.ru>
2020-01-28 13:30:50 +0300

    fix share dod design

--- frontend/widgets/dod/views/dod/index-share.php
+++ frontend/widgets/dod/views/dod/index-share.php
@@ -4,18 +4,36 @@
 /* @var $dod dod\models\DateDod */
 ?>
 <?php if ($model) : ?>
-    <?php foreach ($model as $dod) : ?>
-        <div class="row">
-            <div class="col-md-12 mt-10 dod_share">
-                <div>
-                    <h3 align="center"><?= $dod->dodOne->name ?></h3>
-                    <p><i><?= $dod->dateStartString ?></i></p>
-                    <p><i><?= $dod->timeStartString ?></i></p>
-                    <p><?= $dod->dodOne->addressAndAudNumberString ?></p>
-                    <?= $dod->dodOne->description ?>
-                    <?= \frontend\widgets\dod\UserDodWidget::widget(['dod_id' => $dod->id]); ?>
+    <?php $modelCount = count($model);
+    switch ($modelCount) {
+        case 2:
+            $md = 6;
+            break;
+        case 3;
+            $md = 4;
+            break;
+        case 4;
+            $md = 3;
+            break;
+        default:
+            $md = 12;
+
+    }
+
+    ?>
+    <div class="row">
+        <?php foreach ($model as $dod) : ?>
+                <div class="col-md-<?=$md?>">
+                    <div class="dod_share">
+                        <h3 align="center"><?= $dod->dodOne->name ?></h3>
+                        <p><i> <?= $dod->dateStartString ?></i></p>
+                        <p><i> <?= $dod->timeStartString ?></i></p>
+                        <p> <?= $dod->dodOne->addressAndAudNumberString ?></p>
+                        <?= $dod->dodOne->description ?>
+                        <?= \frontend\widgets\dod\UserDodWidget::widget(['dod_id' => $dod->id]); ?>
+                    </div>
                 </div>
-            </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 <?php endif; ?>
+
