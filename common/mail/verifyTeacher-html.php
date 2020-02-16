6fdfbf295ff38e692795385167f46e1ca5a1336b
Mark <markpdd13@list.ru>
2020-02-17 00:48:05 +0300

    Email and Gratitude

--- common/mail/verifyTeacher-html.php
+++ common/mail/verifyTeacher-html.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use  yii\helpers\Url;
+use olympic\helpers\auth\ProfileHelper;
+use dictionary\helpers\DictSchoolsHelper;
 
 /* @var $this yii\web\View */
 /* @var $teacher teacher\models\UserTeacherJob */
@@ -10,9 +12,18 @@ use  yii\helpers\Url;
 $verifyLink = Url::to(['@frontendInfo/auth/confirm/teacher', 'hash' => $teacher->hash],  true);
 ?>
 <div class="verify-email">
-    <p>Здравствуйте!</p>
+    <p>Уважаемые коллеги! </p>
+    <p>Оргкомитет олимпиад и конкурсов Московского педагогического государственного университета просит подтвердить информацию о том,
+    что <?= ProfileHelper::profileFullName($teacher->user_id)?> работает в <? DictSchoolsHelper::schoolName($teacher->school_id)?> учителем. Данное подтверждение нам необходимо для выдачи электронного благодарственного письма!
+    Для автоматического подтверждения просим пройти по данной ссылке <?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
+    <p>В противном случае просим проигнорировать данное сообщение. </p>
 
-    <p>Для подтвеждения электронной почты, пожалуйста, пройдите по ссылке ниже:</p>
+    <p>Письмо сгенерировано автоматически и отвечать на него не нужно.</p>
 
-    <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
+    <p>С уважением и с благодарностью за сотрудничество,
+    Оргкомитет олимпиад и конкурсов МПГУ
+    Сайт олимпиад sdo.mpgu.org/olimpiads
+    Сайт университета: mpgu.su
+        Телефон для справок: 8(499)702-41-41</p>
+
 </div>
--- common/mail/verifyTeacher-text.php
+++ common/mail/verifyTeacher-text.php
@@ -1,6 +1,8 @@
 <?php
 
 use  yii\helpers\Url;
+use olympic\helpers\auth\ProfileHelper;
+use dictionary\helpers\DictSchoolsHelper;
 
 /* @var $this yii\web\View */
 /* @var $teacher teacher\models\UserTeacherJob */
@@ -8,7 +10,17 @@ use  yii\helpers\Url;
 
 $verifyLink = Url::to(['@frontendInfo/auth/confirm/teacher', 'hash' => $teacher->hash],  true);
 ?>
-Здравствуйте!
+Уважаемые коллеги!
+Оргкомитет олимпиад и конкурсов Московского педагогического государственного университета просит подтвердить информацию о том,
+что <?= ProfileHelper::profileFullName($teacher->user_id)?> работает в <? DictSchoolsHelper::schoolName($teacher->school_id)?> учителем. Данное подтверждение нам необходимо для выдачи электронного благодарственного письма!
+Для автоматического подтверждения просим пройти по данной ссылке <?= $verifyLink ?>
+В противном случае просим проигнорировать данное сообщение.
 
-Для подтвеждения электронной почты, пожалуйста, пройдите по ссылке ниже:
-<?= $verifyLink ?>
+Письмо сгенерировано автоматически и отвечать на него не нужно.
+
+С уважением и с благодарностью за сотрудничество,
+Оргкомитет олимпиад и конкурсов МПГУ
+Сайт олимпиад sdo.mpgu.org/olimpiads
+Сайт университета: mpgu.su
+Телефон для справок: 8(499)702-41-41
+
