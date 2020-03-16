13008edf4aec330e68c53413d5525f74ed244571
Mark <markpdd13@list.ru>
2020-03-16 15:59:39 +0300

    Dod Flash

--- dod/repositories/UserDodRepository.php
+++ dod/repositories/UserDodRepository.php
@@ -9,9 +9,16 @@ class UserDodRepository
     public function get($dod_id, $user_id): UserDod
     {
         if (!$model = UserDod::findOne(['dod_id' => $dod_id, 'user_id' =>$user_id])) {
-            throw new \DomainException('UserDod не найдено.');
+            throw new \DomainException('Ваша запись не существует.');
         }
         return $model;
+    }
+
+    public function getDodUser($dod_id, $user_id): void
+    {
+        if (UserDod::findOne(['dod_id' => $dod_id, 'user_id' =>$user_id])) {
+            throw new \DomainException('На данное мерприятие вы записались.');
+        }
     }
 
     public function save(UserDod $model): void
