bd84207742047022d2428a5b7552b4c3b3061bf0
RamilYumaev <ramilka06@inbox.ru>
2020-03-28 22:07:02 +0300

    Update m191208_00001033_add_dict_category.php

--- modules/entrant/migrations/m191208_00001033_add_dict_category.php
+++ modules/entrant/migrations/m191208_00001033_add_dict_category.php
@@ -4,25 +4,25 @@ use \yii\db\Migration;
 
 class m191208_000010_add_dict_category extends Migration
 {
-   private function table() {
-       return \modules\entrant\models\DictCategory::tableName();
-   }
+    private function table() {
+        return \modules\dictionary\models\DictCategory::tableName();
+    }
 
-   public function up()
-   {
-       $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-       $this->createTable($this->table(), [
-           'id' => $this->primaryKey(),
-           'name' => $this->string()->notNull(),
-           'foreigner_status' => $this->integer(3)->null()->comment('Иностранные граждане'),
-       ], $tableOptions);
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        $this->createTable($this->table(), [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'foreigner_status' => $this->integer(3)->null()->comment('Иностранные граждане'),
+        ], $tableOptions);
 
-       $this->createIndex('{{%idx-dict-category-name}}', $this->table(), 'name');
+        $this->createIndex('{{%idx-dict-category-name}}', $this->table(), 'name');
 
-   }
+    }
 
-   public function down()
-   {
-       $this->dropTable($this->table());
-   }
+    public function down()
+    {
+        $this->dropTable($this->table());
+    }
 }
