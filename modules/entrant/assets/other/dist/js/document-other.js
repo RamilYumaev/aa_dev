56ec34dcd6b85c06eb714efdf4f1543e4637635c
Mark <markpdd13@list.ru>
2020-04-07 15:58:21 +0300

    Agreement as Anketa

--- modules/entrant/assets/other/dist/js/document-other.js
+++ modules/entrant/assets/other/dist/js/document-other.js
@@ -1,12 +1,15 @@
 var otherDocFull = $("#other-document-full");
+var otherAmount = $("div.field-otherdocumentform-amount");
 var selectType = $('#otherdocumentform-type');
 
 const typePhoto = 45;
 selectType.on("change init", function() {
     if (this.value == typePhoto) {
         otherDocFull.hide();
+        otherAmount.show();
     } else {
         otherDocFull.show();
+        otherAmount.hide();
     }
 });
 selectType.trigger("init");
