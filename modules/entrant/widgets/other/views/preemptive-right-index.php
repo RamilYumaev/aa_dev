7360be4354a296b3ccfb816727848cc95b7def6b
Mark <markpdd13@list.ru>
2020-05-10 15:40:53 +0300

    Fix

--- modules/entrant/widgets/other/views/preemptive-right-index.php
+++ modules/entrant/widgets/other/views/preemptive-right-index.php
@@ -3,14 +3,14 @@ use modules\dictionary\helpers\DictDefaultHelper;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model array */
+/* @var $model yii\db\BaseActiveRecord */
 
 ?>
 <div class="row">
     <div class="col-md-12">
         <h4>Преимущественное право</h4>
         <?= Html::a("Добавить/Редактировать", ["preemptive-right/index"], ["class" => "btn btn-success"]) ?>
-        <?php if($model): ?>
+        <?php if($model):?>
         <table class="table table-bordered">
             <tr>
                 <th>Приоритет</th>
@@ -18,8 +18,8 @@ use yii\helpers\Html;
             </tr>
             <?php foreach ($model as $item) : ?>
                 <tr>
-                    <td><?= $item['type_id'] ?></td>
-                    <td><?= DictDefaultHelper::preemptiveRightName($item['type_id']) ?></td>
+                    <td><?= $item->type_id ?></td>
+                    <td><?= DictDefaultHelper::preemptiveRightName($item->type_id) ?></td>
                 </tr>
             <?php  endforeach; ?>
         </table>
