67493cddeb8ba3a31741871392e3a62aae9ef1dd
Ramil <ramilka06@inbox.ru>
2020-05-14 00:39:35 +0300

    fix

--- modules/entrant/widgets/education/views/file.php
+++ modules/entrant/widgets/education/views/file.php
@@ -4,9 +4,10 @@ use modules\entrant\widgets\file\FileWidget;
 use modules\entrant\widgets\file\FileListWidget;
 /* @var $model modules\entrant\models\DocumentEducation*/
 ?>
+<h3>Скан документа об образовании</h3>
 <table class="table table-bordered">
     <tr>
-        <th>Данные документа об образавании</th>
+        <th>Документ об образовании</th>
         <th><?= FileWidget::widget(['record_id' => $model->id, 'model' =>$model::className() ]) ?></th>
     </tr>
     <tr>
