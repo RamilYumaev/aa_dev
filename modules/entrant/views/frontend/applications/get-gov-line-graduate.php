98d016680ed652d5eb12d68655fae7bf20306f8d
Ramil <ramilka06@inbox.ru>
2020-05-27 17:40:00 +0300

    fix

--- modules/entrant/views/frontend/applications/get-gov-line-graduate.php
+++ modules/entrant/views/frontend/applications/get-gov-line-graduate.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * @var $currentFaculty;
+ * @var $currentFaculty ;
  */
 
 use dictionary\helpers\DictCompetitiveGroupHelper;
 use dictionary\models\DictCompetitiveGroup;
+use modules\dictionary\models\CathedraCg;
 use modules\entrant\helpers\UserCgHelper;
 use yii\helpers\Html;
 use yii\web\View;
@@ -19,63 +20,78 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $result = "";
 
-foreach ($currentFaculty as $faculty) {
+$currentYear = Date("Y");
+$lastYear = $currentYear - 1;
 
-    $cgFaculty = DictCompetitiveGroup::find()
-        ->eduLevel(DictCompetitiveGroupHelper::EDUCATION_LEVEL_GRADUATE_SCHOOL)
-        ->budgetOnly()
-        ->foreignerStatus(true)
-        ->currentAutoYear()
-        ->faculty($faculty)
-        ->orderBy(['education_form_id' => SORT_ASC, 'speciality_id' => SORT_ASC])
-        ->all();
+if($currentFaculty) {
 
-    if ($cgFaculty) {
+    foreach ($currentFaculty as $faculty) {
 
-        $result .= "<h3 class=\"text-center\">" . \dictionary\helpers\DictFacultyHelper::facultyList()[$faculty] . "</h3>";
-        $result .=
-            "<table class=\"table tabled-bordered\">
+        $cgFaculty = CathedraCg::find()
+            ->innerJoinWith('cathedra')
+            ->innerJoinWith('competitiveGroup')
+            ->andWhere([DictCompetitiveGroup::tableName() . '.`faculty_id`' => $faculty])
+            ->andWhere([DictCompetitiveGroup::tableName() . '.`financing_type_id`' => DictCompetitiveGroupHelper::FINANCING_TYPE_BUDGET])
+            ->andWhere([DictCompetitiveGroup::tableName() . '.`foreigner_status`' => 1])
+            ->andWhere([DictCompetitiveGroup::tableName() . '.`edu_level`' => DictCompetitiveGroupHelper::EDUCATION_LEVEL_GRADUATE_SCHOOL])
+            ->andWhere([DictCompetitiveGroup::tableName() . '.`year`' => "$lastYear-$currentYear"])
+            ->orderBy([DictCompetitiveGroup::tableName() . '.`education_form_id`' => SORT_ASC,
+                DictCompetitiveGroup::tableName() . '.`speciality_id`' => SORT_ASC])
+            ->all();
+
+        if ($cgFaculty) {
+
+            $result .= "<h3 class=\"text-center\">" . \dictionary\helpers\DictFacultyHelper::facultyList()[$faculty] . "</h3>";
+            $result .=
+                "<table class=\"table tabled-bordered\">
 <tr>
 <th>Код, Направление подготовки, профиль</th>
+<th>Кафедра</th>
 <th>Форма и срок обучения</th>
 <th>Уровень образования</th>
 <th</th>
 </tr>";
+            foreach ($cgFaculty as $currentCg) {
 
-        foreach ($cgFaculty as $currentCg) {
+                $trColor = UserCgHelper::trColor($currentCg->competitiveGroup);
+                $result .= "<tr" . $trColor . ">";
 
-            $trColor = UserCgHelper::trColor($currentCg);
-            $result .= "<tr" . $trColor . ">";
+                $result .= "<td>";
+                $result .= $currentCg->competitiveGroup->specialty->getCodeWithName();
+                $result .= $currentCg->competitiveGroup->specialization ? ", профиль(-и) <strong>" . $currentCg->competitiveGroup->specialization->name
+                    . "</strong>" : "";
+                $result .= "</td>";
 
-            $result .= "<td>";
-            $result .= $currentCg->specialty->getCodeWithName();
-            $result .= $currentCg->specialization ? ", профиль(-и) <strong>" . $currentCg->specialization->name
-                . "</strong>" : "";
-            $result .= "</td>";
+                $result .= "<td>";
+                $result .= $currentCg->cathedra->name;
+                $result .= "</td>";
+
+                $result .= "<td>";
+                $result .= DictCompetitiveGroupHelper::getEduForms()[$currentCg->competitiveGroup->education_form_id] . ", ";
+                $result .= $currentCg->competitiveGroup->education_duration != 5 ? $currentCg->competitiveGroup->education_duration . " года"
+                    : $currentCg->competitiveGroup->education_duration . " лет";
+                $result .= "</td>";
 
 
-            $result .= "<td>";
-            $result .= DictCompetitiveGroupHelper::getEduForms()[$currentCg->education_form_id] . ", ";
-            $result .= $currentCg->education_duration != 5 ? $currentCg->education_duration . " года"
-                : $currentCg->education_duration . " лет";
-            $result .= "</td>";
+                $result .= "<td>";
+                $result .= DictCompetitiveGroupHelper::eduLevelName($currentCg->competitiveGroup->edu_level);
+                $result .= "</td>";
 
+                $result .= "<td>";
+                $result .= UserCgHelper::link($currentCg->competitiveGroup->id, DictCompetitiveGroupHelper::FINANCING_TYPE_BUDGET);
+                $result .= "</td>";
+                $result .= "</tr>";
+            }
 
-            $result .= "<td>";
-            $result .= DictCompetitiveGroupHelper::eduLevelName($currentCg->edu_level);
-            $result .= "</td>";
-
-            $result .= "<td>";
-            $result .= UserCgHelper::link($currentCg->id, DictCompetitiveGroupHelper::FINANCING_TYPE_BUDGET);
-            $result .= "</td>";
-            $result .= "</tr>";
+        }else {
+            continue;
         }
-
     }
 }
+    $result .= "</table>";
 
 ?>
-    <?php Pjax::begin(['id' => 'get-gov-line-bachelor', 'timeout' => false, 'enablePushState' => false]); ?>
+<?php Pjax::begin(['id' => 'get-bachelor', 'timeout' => false, 'enablePushState' => false]); ?>
     <div class="row min-scr">
         <div class="button-left">
             <?= Html::a(Html::tag("span", "", ["class" => "glyphicon glyphicon-arrow-left"]), ["anketa/step2"], ["class" => "btn btn-lg btn-warning position-fixed"]); ?>
@@ -84,21 +100,22 @@ foreach ($currentFaculty as $faculty) {
             <?= Html::a(Html::tag("span", "", ["class" => "glyphicon glyphicon-arrow-right"]), ["/abiturient"], ["class" => "btn btn-lg btn-success position-fixed"]); ?>
         </div>
     </div>
-    <h2 class="text-center mt-50"><?= $this->title ?></h2>
+    <h2 class="text-center"><?= $this->title ?></h2>
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <?= Html::img("/img/cabinet/btn-budget-plus.png", ["width"=>"23px", "height"=> "20px"]) ?>
+                <?= Html::img("/img/cabinet/btn-budget-plus.png", ["width" => "23px", "height" => "20px"]) ?>
                 - кнопка выбора образовательной программы на бюджетной основе.<br/><br/>
-                <?= Html::img("/img/cabinet/btn-budget-minus.png", ["width"=>"23px", "height"=> "20px"])?>
+                <?= Html::img("/img/cabinet/btn-budget-minus.png", ["width" => "23px", "height" => "20px"]) ?>
                 - кнопка отмены выбора образовательной программы на бюджетной основе.
             </div>
         </div>
-    <div class="table-responsive">
-        <?= $result ?>
+        <div class="table-responsive">
+            <?= $result ?>
+        </div>
     </div>
-    </div>
-    <?php Pjax::end(); ?>
+
+<?php Pjax::end(); ?>
 
 <?php
 $this->registerJs("
