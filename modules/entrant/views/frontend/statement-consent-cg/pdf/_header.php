92f1c75359a8adafb70981e42d77cba76f78e4a2
Ramil <ramilka06@inbox.ru>
2020-05-30 01:11:23 +0300

    fix

--- modules/entrant/views/frontend/statement-consent-cg/pdf/_header.php
+++ modules/entrant/views/frontend/statement-consent-cg/pdf/_header.php
@@ -6,6 +6,7 @@ use modules\entrant\helpers\AddressHelper;
 use modules\entrant\helpers\DocumentEducationHelper;
 use \olympic\helpers\auth\ProfileHelper;
 use yii\helpers\Html;
+
 /* @var $user_id integer */
 /* @var  $profile array */
 /* @var  $name string */
@@ -15,14 +16,16 @@ $passport = PassportDataHelper::dataArray($user_id);
 
 ?>
 <table class="fs-15">
-    <tr><td width="62%"></td>
+    <tr>
+        <td width="62%"></td>
         <td>
             Председателю Приемной<br/>
             комиссии МПГУ,<br/>
             ректору МПГУ,<br/>
             А.В. Лубкову<br/>
-            От: <?=$name->genitive ?? ""?><br/>
-            тел.: <?=$profile['phone']?>
+            От: <?= $name->genitive ?? $profile['last_name'] . " " . $profile['first_name'] . " ".$profile['patronymic']?>
+            <br/>
+            тел.: <?= $profile['phone'] ?>
         </td>
     </tr>
 </table>
