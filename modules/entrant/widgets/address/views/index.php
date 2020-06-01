96538b127aeb13d64eae698f966888cf23d02b09
Ramil <ramilka06@inbox.ru>
2020-06-02 00:27:43 +0300

    fix#2

--- modules/entrant/widgets/address/views/index.php
+++ modules/entrant/widgets/address/views/index.php
@@ -12,8 +12,8 @@ use modules\entrant\helpers\AddressHelper;
     <div class="col-md-12 <?= BlockRedGreenHelper::colorBg(AddressHelper::isExits($userId)) ?>">
         <div class="p-30 green-border">
         <h4>Адреса регистрации и проживания:</h4>
-            <p> <span class="badge bg-red">В этом блоке необходимо обязательно указать фактический адрес Вашего
-                    проживания и адрес регистрации (постоянной и/временной)</span></p>
+            <p> <span class="badge bg-red-light">В этом блоке необходимо обязательно указать фактический адрес Вашего
+                    проживания и адрес регистрации (постоянной и/или временной)</span></p>
         <?= Html::a('Добавить', ['address/create'], ['class' => 'btn btn-success mb-10']) ?>
         <?= \yii\grid\GridView::widget([
             'tableOptions' => ['class' => 'table  table-bordered'],
--- modules/entrant/widgets/passport/views/index.php
+++ modules/entrant/widgets/passport/views/index.php
@@ -13,6 +13,7 @@ use modules\entrant\helpers\BlockRedGreenHelper;
     <div class="col-md-12">
         <div class="p-30 mt-10 green-border">
             <h4>Иные документы, удостоверяющие личность</h4>
+
             <?= Html::a('Добавить', ['passport-data/create'], ['class' => 'btn btn-success mb-10']) ?>
             <?= \yii\grid\GridView::widget([
                 'tableOptions' => ['class' => 'table  table-bordered'],
