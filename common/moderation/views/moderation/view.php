06f12dd6850d23f1c4cba2866e8797b8e736b872
Ramil <ramilka06@inbox.ru>
2020-06-03 15:19:32 +0300

    fix

--- common/moderation/views/moderation/view.php
+++ common/moderation/views/moderation/view.php
@@ -1,18 +1,21 @@
 <?php
 
+use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $moderation common\moderation\models\Moderation*/
+/* @var $moderation common\moderation\models\Moderation */
 
-$this->title =  "Модерация. ". $moderation->getModel()->titleModeration();
+$this->title = "Модерация. " . $moderation->getModel()->titleModeration();
 $this->params['breadcrumbs'][] = ['label' => "Модерация", 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-if($moderation->model !== \dictionary\models\DictSchools::class ||
-$moderation->model !==\modules\dictionary\models\DictOrganizations::class ) {
+if ($moderation->model !== \dictionary\models\DictSchools::class ||
+    $moderation->model !== \modules\dictionary\models\DictOrganizations::class) {
     $modelData = $moderation->model::findOne($moderation->record_id);
-    if($modelData) {
-    echo $modelData->user_id;
+    if ($modelData) {
+        echo Html::a("Войти в кабинет пользователя", \Yii::$app->params['staticHostInfo'] . '/switch-user/by-user-id?id=' . $modelData->user_id,
+            ['class' => 'btn btn-info', 'target' => '_blank']);
+
     }
 }
 ?>
@@ -20,23 +23,23 @@ $moderation->model !==\modules\dictionary\models\DictOrganizations::class ) {
     <div class="box">
         <div class="box-body">
             <?= DetailView::widget(['model' => $moderation,
-             // 'formatter' => ['class' => 'yii\i18n\Formatter','timeZone' => 'Europe/Moscow'],
-             'attributes' => [
-                      'model',
-                      'record_id',
-                      'message:html',    // description attribute in HTML
-                 [ 'label' => $moderation->getAttributeLabel('created_by'),
-                     'value' =>\olympic\helpers\auth\ProfileHelper::profileShortName($moderation->created_by)],
-                 [ 'label' => $moderation->getAttributeLabel('status'),
-                     'value' => \common\moderation\helpers\ModerationHelper::statusName($moderation->status)],
-                 [ 'label' => $moderation->getAttributeLabel('moderated_by'),
-                     'value' =>\olympic\helpers\auth\ProfileHelper::profileShortName($moderation->moderated_by)],
-                  'created_at:datetime',
-                  'updated_at:datetime',
-                 ],]) ?>
+                // 'formatter' => ['class' => 'yii\i18n\Formatter','timeZone' => 'Europe/Moscow'],
+                'attributes' => [
+                    'model',
+                    'record_id',
+                    'message:html',    // description attribute in HTML
+                    ['label' => $moderation->getAttributeLabel('created_by'),
+                        'value' => \olympic\helpers\auth\ProfileHelper::profileShortName($moderation->created_by)],
+                    ['label' => $moderation->getAttributeLabel('status'),
+                        'value' => \common\moderation\helpers\ModerationHelper::statusName($moderation->status)],
+                    ['label' => $moderation->getAttributeLabel('moderated_by'),
+                        'value' => \olympic\helpers\auth\ProfileHelper::profileShortName($moderation->moderated_by)],
+                    'created_at:datetime',
+                    'updated_at:datetime',
+                ],]) ?>
         </div>
     </div>
     <div>
-        <?= \common\moderation\widgets\ModerationWidget::widget(['moderation'=> $moderation]); ?>
+        <?= \common\moderation\widgets\ModerationWidget::widget(['moderation' => $moderation]); ?>
     </div>
 </div>
