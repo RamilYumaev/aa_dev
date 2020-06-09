bf998b292eb5f38ba672996bc0b6562b12f5b459
Ramil <ramilka06@inbox.ru>
2020-06-09 11:21:06 +0300

    fix

--- modules/entrant/widgets/statement/views/statement-index.php
+++ modules/entrant/widgets/statement/views/statement-index.php
@@ -10,36 +10,38 @@ use \yii\bootstrap\Collapse;
 
 ?>
 <?php if ($statements): ?>
+    <div class="panel panel-default">
+        <div class="panel-heading"><h4>Заявления об участии в конкурсе</h4></div>
+        <div class="panel-body">
+            <?php
+            $result = [];
+            $resultFinish = [];
+            $array = [];
+            foreach ($statements as $statement) {
+                $resultData = "<table class='table table-bordered'>";
+                $resultData .= "<th>Образовательные программы</th>";
+                foreach ($statement->statementCg as $statementCg) {
+                    $resultData .= "<tr>";
+                    $resultData .= "<td>";
+                    $resultData .= $statementCg->cg->fullName;
+                    $resultData .= "</td>";
+                    $resultData .= "</tr>";
+                }
+                $resultData .= "</table>";
+                $result = [
+                    ['label' => $statement->faculty->full_name . ", " . $statement->speciality->getCodeWithName() . ",<br/>Заявление № "
+                        . $statement->numberStatement . " <span class=\"label label-"
+                        . StatementHelper::colorName($statement->status) . "\">" . $statement->statusName . "</span>",
+                        'content' => $resultData,
+                        'contentOptions' => ['class' => 'out']],];
 
-    <?php
-    $result = [];
-    $resultFinish = [];
-    $array = [];
-    foreach ($statements as $statement) {
-        $resultData = "<table class='table table-bordered'>";
-        $resultData .= "<th>Образовательные программы</th>";
-        foreach ($statement->statementCg as $statementCg) {
-            $resultData .= "<tr>";
-            $resultData .= "<td>";
-            $resultData .= $statementCg->cg->fullName;
-            $resultData .= "</td>";
-            $resultData .= "</tr>";
-        }
-        $resultData .= "</table>";
-        $result = [
-            ['label' => $statement->faculty->full_name.", ".$statement->speciality->getCodeWithName().",<br/>Заявление № "
-                . $statement->numberStatement . " <span class=\"label label-"
-                . StatementHelper::colorName($statement->status) . "\">" . $statement->statusName . "</span>",
-            'content' => $resultData,
-            'contentOptions' => ['class' => 'out']],];
-
-        if(count($array))
-        {
-            $resultFinish = array_merge($array, $result);
-        }
-        $array = $result;
-    }
-    ?>
-    <?=Collapse::widget(['encodeLabels' => false, 'items' => $resultFinish]); ?>
-
+                if (count($array)) {
+                    $resultFinish = array_merge($array, $result);
+                }
+                $array = $result;
+            }
+            ?>
+            <?= Collapse::widget(['encodeLabels' => false, 'items' => $resultFinish]); ?>
+        </div>
+    </div>
 <?php endif; ?>
