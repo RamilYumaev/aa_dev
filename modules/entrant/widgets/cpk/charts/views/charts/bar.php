efe950450689bfdaea2769ffac89326999033ef4
Mark <markpdd13@list.ru>
2020-06-09 11:49:59 +0300

    Fix

--- modules/entrant/widgets/cpk/charts/views/charts/bar.php
+++ modules/entrant/widgets/cpk/charts/views/charts/bar.php
@@ -8,7 +8,7 @@
 ?>
 <div class="box box-success">
             <div class="box-header with-border">
-              <h5 class="box-title"> <?= $cg->getFullNameV() ?></h5>
+              <p class="box-title"> <?= $cg->getFullNameV() ?></p>
 
 <div class="box-tools pull-right">
     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
