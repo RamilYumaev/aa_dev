59bb72f3c16cb1aca7dab9c66ed8aeb64b50998b
Ramil <ramilka06@inbox.ru>
2020-06-14 12:02:46 +0300

    fix

--- modules/entrant/views/frontend/post-document/index.php
+++ modules/entrant/views/frontend/post-document/index.php
@@ -27,16 +27,18 @@ $userId =  Yii::$app->user->identity->getId();
     <h1 align="center"><?= $this->title ?></h1>
     <div class="row">
         <div class="col-md-12">
+            <?= \modules\entrant\widgets\statement\StatementPersonalDataWidget::widget(['userId' => $userId]); ?>
+
             <?= \modules\entrant\widgets\passport\PassportMainWidget::widget(['view' => 'file', 'userId' => $userId]); ?>
+
+            <?php if (!$anketa->isNoRequired()): ?>
+                <?= \modules\entrant\widgets\address\AddressFileWidget::widget(['userId' => $userId]); ?>
+            <?php endif; ?>
 
             <?= \modules\entrant\widgets\education\DocumentEducationFileWidget::widget(['userId' => $userId]); ?>
 
             <?php if ($anketa->isAgreement()): ?>
                 <?= \modules\entrant\widgets\agreement\AgreementWidget::widget(['view' => 'file', 'userId' => $userId]); ?>
-            <?php endif; ?>
-
-            <?php if (!$anketa->isNoRequired()): ?>
-                <?= \modules\entrant\widgets\address\AddressFileWidget::widget(['userId' => $userId]); ?>
             <?php endif; ?>
 
             <?= \modules\entrant\widgets\other\DocumentOtherFileWidget::widget(['userId' => $userId]); ?>
@@ -47,12 +49,9 @@ $userId =  Yii::$app->user->identity->getId();
             <?= \modules\entrant\widgets\submitted\SubmittedDocumentGenerateStatementWidget::widget(['userId' => $userId,
                 'formCategory' =>DictCompetitiveGroupHelper::FORM_EDU_CATEGORY_2]); ?>
 
-            <?= \modules\entrant\widgets\statement\StatementIaWidget::widget(['userId' => $userId]); ?>
-
-            <?= \modules\entrant\widgets\statement\StatementPersonalDataWidget::widget(['userId' => $userId]); ?>
-
             <?= \modules\entrant\widgets\statement\StatementCgConsentWidget::widget(['userId' => $userId]); ?>
 
+            <?= \modules\entrant\widgets\statement\StatementIaWidget::widget(['userId' => $userId]); ?>
         </div>
     </div>
     <div class="row mb-30">
