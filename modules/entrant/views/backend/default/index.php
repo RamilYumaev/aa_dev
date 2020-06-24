47329ef9df84c784ee12e5b0c40a9ecd904dfef5
Ramil <ramilka06@inbox.ru>
2020-06-24 17:46:53 +0300

    fix5

--- modules/entrant/views/backend/default/index.php
+++ modules/entrant/views/backend/default/index.php
@@ -8,21 +8,22 @@ use modules\entrant\helpers\SelectDataHelper;
 use olympic\helpers\auth\ProfileHelper;
 use yii\grid\GridView;
 use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 /* @var $type integer */
 /* @var $searchModel modules\entrant\searches\ProfilesStatementSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 
-$typeString = $type ? " (".AisReturnDataHelper::statusName($type).")" : "";
-$this->title = 'Абитуриенты'.$typeString;
+$typeString = $type ? " (" . AisReturnDataHelper::statusName($type) . ")" : "";
+$this->title = 'Абитуриенты' . $typeString;
 
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div>
     <div class="box">
-        <div class="box-header"><?= $type && $type == AisReturnDataHelper::AIS_NO ? Html::a("Excel Юзер Id ",['excel'],['class'=> "btn btn-success"]): ""?></div>
+        <div class="box-header"><?= $type && $type == AisReturnDataHelper::AIS_NO ? Html::a("Excel Юзер Id ", ['excel'], ['class' => "btn btn-success"]) : "" ?></div>
         <div class="box-body table-responsive">
             <?= GridView::widget([
                 'dataProvider' => $dataProvider,
@@ -31,25 +32,33 @@ $this->params['breadcrumbs'][] = $this->title;
                     ['class' => \yii\grid\SerialColumn::class],
                     'user_id',
                     'last_name', 'first_name', 'patronymic',
-                    ['attribute'=>'gender',
-                      'value' => 'genderName',
+                    ['attribute' => 'gender',
+                        'value' => 'genderName',
                         'filter' => ProfileHelper::typeOfGender()
-                        ],
-                    [  'attribute'=>'country_id',
+                    ],
+                    ['attribute' => 'country_id',
                         'value' => 'countryName',
                         'filter' => SelectDataHelper::dataSearchModel($searchModel, DictCountryHelper::countryListMap(), 'country_id', 'countryName')
                     ],
-                    ['attribute'=> 'region_id',
+                    ['attribute' => 'region_id',
                         'value' => 'regionName',
                         'filter' => SelectDataHelper::dataSearchModel($searchModel, DictRegionHelper::regionList(), 'region_id', 'regionName')
 
                     ],
                     'phone',
-                    ['value' => function($model) {
-                      return Html::a("Просмотр", ['full', 'user' => $model->user_id], ['class' => 'btn btn-info']);
-                      },
-                      'format' => "raw",
-                      ]
+                    [
+                            'header'=> 'Статус загрузки в АИС ВУЗ',
+                            'value' => function (\olympic\models\auth\Profiles $model) {
+                        return $model->aisUser ? Html::tag("span", "загружен", ['class' => "label label-success"])
+                            : Html::tag("span", "не загружен", ['class' => "label label-danger"]);
+                    },
+                        'format' => "raw",],
+                    ['value' => function ($model) {
+                        return Html::a("Просмотр", ['full', 'user' => $model->user_id], ['class' => 'btn btn-info']);
+
+                    },
+                        'format' => "raw",
+                    ]
                 ]
             ]); ?>
         </div>
