358e02d854a86b90bc0d3c0dbe97dce20f3db764
Ramil <ramilka06@inbox.ru>
2020-06-27 23:06:35 +0300

    fix_moderation

--- common/moderation/views/moderation/view.php
+++ common/moderation/views/moderation/view.php
@@ -5,19 +5,29 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $moderation common\moderation\models\Moderation */
+/* @var $userId integer */
 
 $this->title = "Модерация. " . $moderation->getModel()->titleModeration();
 $this->params['breadcrumbs'][] = ['label' => "Модерация", 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-if ($moderation->model !== \dictionary\models\DictSchools::class ||
+/*if ($moderation->model !== \dictionary\models\DictSchools::class ||
     $moderation->model !== \modules\dictionary\models\DictOrganizations::class) {
     $modelData = $moderation->model::findOne($moderation->record_id);
+
     if ($modelData) {
         echo Html::a("Войти в кабинет пользователя", \Yii::$app->params['staticHostInfo'] . '/switch-user/by-user-id?id=' . $modelData->user_id,
             ['class' => 'btn btn-info', 'target' => '_blank']);
 
     }
+
+}*/
+
+if($userId)
+{
+    echo Html::a("Войти в кабинет пользователя", \Yii::$app->params['staticHostInfo'] . '/switch-user/by-user-id?id=' . $userId,
+        ['class' => 'btn btn-info', 'target' => '_blank']);
 }
+
 ?>
 <div id="moderation-view">
     <div class="box">
