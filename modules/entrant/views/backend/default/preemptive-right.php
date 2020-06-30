8ba7de95d968010c1ea5378681f46eec02bbd0ab
Ramil <ramilka06@inbox.ru>
2020-06-30 16:47:56 +0300

    fix1

--- modules/entrant/views/backend/default/preemptive-right.php
+++ modules/entrant/views/backend/default/preemptive-right.php
@@ -4,12 +4,13 @@ use entrant\assets\modal\ModalAsset;
 use yii\helpers\Html;
 /* @var $profile olympic\models\auth\Profiles */
 ModalAsset::register($this);
+$userId = $profile->user_id;
 
 $this->title = 'Преимущественное право';
 $this->params['breadcrumbs'][] = ['label' => 'Абитуриенты', 'url' => ['default/index']];
-$this->params['breadcrumbs'][] = ['label' => $profile->getFio() .'. Персональная карточка поступающего', 'url' => ['default/full']];
+$this->params['breadcrumbs'][] = ['label' => $profile->getFio() .'. Персональная карточка поступающего',
+    'url' => ['default/full', 'user'=>$userId]];
 $this->params['breadcrumbs'][] = $this->title;
 
-$userId = $profile->user_id;
 ?>
 <?= \modules\entrant\widgets\other\PreemptiveRightIndexWidget::widget(['userId' => $userId, 'view' => 'preemptive-right-mpgu']) ?>
