b52b6fa1d47c74ddec9d495519ea3f57c2b1065d
Ramil <ramilka06@inbox.ru>
2020-07-13 13:33:43 +0300

    fix_data_export

--- modules/entrant/helpers/DataExportHelper.php
+++ modules/entrant/helpers/DataExportHelper.php
@@ -291,7 +291,10 @@ class DataExportHelper
         $patronymic = "";
         $result['documents'] = [];
 
-        foreach (PassportData::find()->where(['user_id' => $userId])->all() as $currentDocument) {
+        foreach (PassportData::find()
+                     ->andWhere(['user_id' => $userId])
+                     ->orderBy(['main_status'=> SORT_DESC])
+                     ->all() as $currentDocument) {
             $result['documents'][] = [
                 'sdo_id' => $currentDocument->id,
                 'model_type' => 1,
@@ -345,7 +348,9 @@ class DataExportHelper
             ];
         }
 
-        foreach (DocumentEducation::find()->where(['user_id' => $userId])->all() as $currentDocument) {
+        foreach (DocumentEducation::find()
+                     ->andWhere(['user_id' => $userId])
+                     ->all() as $currentDocument) {
             $result['documents'][] = [
                 'sdo_id' => $currentDocument->id,
                 'model_type' => 3,
