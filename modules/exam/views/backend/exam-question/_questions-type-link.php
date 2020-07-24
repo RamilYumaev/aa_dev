98c06537b3b4c76eaa510792333f58fcf4ac8304
Ramil <ramilka06@inbox.ru>
2020-07-24 21:27:30 +0300

    fix5

--- modules/exam/views/backend/exam-question/_questions-type-link.php
+++ modules/exam/views/backend/exam-question/_questions-type-link.php
@@ -7,13 +7,15 @@ use yii\helpers\Html;
     <div class="col-md-12">
         <?= Yii::$app->user->can("multiple-type") ?
             Html::a('Выбрать вариант(ы)', ['create', 'type' =>TestQuestionHelper::TYPE_SELECT], [ 'class'=>'btn btn-success']) : ""; ?>
-        <?= Html::a('Выбрать вариант', ['create', 'type' =>TestQuestionHelper::TYPE_SELECT_ONE], [ 'class'=>'btn btn-success']); ?>
-        <?= Html::a('Сопоставить', ['create', 'type' =>TestQuestionHelper::TYPE_MATCHING], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Тип 5.2.0 Легкий вопрос (да-нет)', ['create', 'type' =>TestQuestionHelper::TYPE_SELECT_ONE], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Тип 5.2.1 Трудный вопрос (один из четырех)', ['create', 'type' =>TestQuestionHelper::TYPE_SELECT_ONE], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Тип 5.2.2  Трудный вопрос (на установление соответствия)', ['create', 'type' =>TestQuestionHelper::TYPE_MATCHING], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Тип 5.2.3 Трудный вопрос (на установление последовательности)', ['create', 'type' =>TestQuestionHelper::TYPE_MATCHING], [ 'class'=>'btn btn-success']); ?>
         <?= Yii::$app->user->can("short-type") ?
             Html::a('Краткий ответ', ['create', 'type' =>TestQuestionHelper::TYPE_ANSWER_SHORT], [ 'class'=>'btn btn-success']) : ""; ?>
         <?= Yii::$app->user->can("detailed-type") ?
             Html::a('Развернутый ответ', ['create', 'type' =>TestQuestionHelper::TYPE_ANSWER_DETAILED], [ 'class'=>'btn btn-success']) : ""; ?>
         <?php /* Html::a('Загрузка файла', ['create', 'type' =>TestQuestionHelper::TYPE_FILE], [ 'class'=>'btn btn-success']); */ ?>
-        <?= Html::a('Вложенные ответы', ['create', 'type' =>TestQuestionHelper::TYPE_CLOZE], [ 'class'=>'btn btn-success']); ?>
+        <?= Html::a('Тип 5.2.4. Трудный вопрос (на заполнение пропусков)', ['create', 'type' =>TestQuestionHelper::TYPE_CLOZE], [ 'class'=>'btn btn-success']); ?>
     </div>
 </div>
