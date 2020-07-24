565a7d5f4af8188ca1a2e46fac26ecffec4c4ba1
Ramil <ramilka06@inbox.ru>
2020-07-24 10:22:54 +0300

    fix_school

--- modules/entrant/behaviors/CgBehavior.php
+++ modules/entrant/behaviors/CgBehavior.php
@@ -4,6 +4,7 @@ namespace modules\entrant\behaviors;
 
 use dictionary\helpers\DictCompetitiveGroupHelper;
 use dictionary\models\DictCompetitiveGroup;
+use modules\entrant\models\Statement;
 use yii\base\Behavior;
 use yii\db\ActiveRecord;
 use modules\entrant\models\UserCg;
@@ -21,6 +22,7 @@ class CgBehavior extends Behavior
     {
         return [
             ActiveRecord::EVENT_BEFORE_UPDATE => 'beforeUpdate',
+            ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
         ];
     }
 
@@ -29,6 +31,19 @@ class CgBehavior extends Behavior
         if ($this->userCgExists() && !$this->checkUpdate()) {
             UserCg::deleteAll(['user_id' => Yii::$app->user->identity->getId(),
                     'cg_id' => $this->bachelorCg()]);
+            Statement::deleteAll(['user_id' => Yii::$app->user->identity->getId(),
+                'edu_level'=>DictCompetitiveGroupHelper::EDUCATION_LEVEL_BACHELOR]);
+        }
+
+    }
+
+    public function beforeDelete($event)
+    {
+        if ($this->userCgExists()) {
+            UserCg::deleteAll(['user_id' => Yii::$app->user->identity->getId(),
+                'cg_id' => $this->bachelorCg()]);
+            Statement::deleteAll(['user_id' => Yii::$app->user->identity->getId(),
+                'edu_level'=>DictCompetitiveGroupHelper::EDUCATION_LEVEL_BACHELOR]);
         }
 
     }
