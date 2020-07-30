2cf2d8ead431de5fab0ec77b444b775181df01eb
Ramil <ramilka06@inbox.ru>
2020-07-30 13:13:32 +0300

    fix

--- modules/exam/views/frontend/default/index.php
+++ modules/exam/views/frontend/default/index.php
@@ -63,7 +63,7 @@ $userId = Yii::$app->user->identity->getId();
                                 <td><?= $examStatement->dateView ?></td>
                                 <td><?= $examStatement->typeName ?></td>
                                 <td><?= $examStatement->statusName ?></td>
-                                <td> <?= $examStatement->src_bbb  && !$examStatement->srcDisabled() ? Html::a("Ссылка на кабинет",  $examStatement->src_bbb  , ["class" => "btn btn-primary", 'target'=>'_blank']) : "" ?></td>
+                                <td> <?= $examStatement->src_bbb  && !$examStatement->srcDisabled() ? Html::a("Комната для прокторинга и получениия допуска",  $examStatement->src_bbb  , ["class" => "btn btn-primary", 'target'=>'_blank']) : "" ?></td>
                             </tr>
                             <?php endforeach; ?>
                         </table>
