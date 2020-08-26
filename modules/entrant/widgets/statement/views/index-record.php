38aba37fde45484a20b1a7a1ca36280fee1562de
Mark <markpdd13@list.ru>
2020-08-26 22:59:01 +0300

    fyx

--- modules/entrant/widgets/statement/views/index-record.php
+++ modules/entrant/widgets/statement/views/index-record.php
@@ -17,6 +17,9 @@ use modules\entrant\widgets\file\FileListWidget;
 ?>
 
 <?php if($orderTransfer) : ?>
+    <p class="label label-warning fs-15">
+        С 01.09 по 13.09 обучение будет проходить дистанционно.  <?= Html::a("Подробнее","http://mpgu.su/podgotovka-k-1-sentjabrja/") ?>
+    </p>
     <div class="panel panel-default">
         <div class="panel-heading"><h4>Приказы о зачислении <?= $isDownload ? "" : Html::a("Исключить", "/abiturient/post-document/rejection-record") ?></h4></div>
         <div class="panel-body">
@@ -36,6 +39,7 @@ use modules\entrant\widgets\file\FileListWidget;
             <?php endforeach; ?>
         </div>
     </div>
+
 <?php endif; ?>
 <?php if($statementRecord) : ?>
     <div class="panel panel-default">
