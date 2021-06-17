94210cfe225f6562d5f916a5781fd896205059b7
Anastasia <anastasiarebr@gmail.com>
2021-06-17 19:54:15 +0300

    Опечатки

--- modules/entrant/views/frontend/statement-personal-data/pdf/_header_ras_rod.php
+++ modules/entrant/views/frontend/statement-personal-data/pdf/_header_ras_rod.php
@@ -28,38 +28,38 @@ if ($cpk) {
     
     <table width="100%">
         <tr>
-            <td><strong>Субъект персональных данных</strong> (абитуриент/обучающийся):</td>
-            <td width="50%"
-                class="bb text-center"><?= $profile['last_name'] ?> <?= $profile['first_name'] ?> <?= $profile['patronymic'] ? " "
-                    . $profile['patronymic'] : "" ?></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td colspan="1" class="v-align-top text-center fs-7"><i>(фамилия, имя, отчество, дата рождения
-                    поступающего)</i></td>
-            <td></td>
+            <td><strong>Субъект персональных данных</strong> (родитель, законный представитель обучающегося, абитуриента):</td>
         </tr>
     </table>
     <table width="100%">
         <tr>
-            <td class="bb"><?= $passport['date_of_birth'] ?></td>
+            <td class="bb text-center h-20"></td>
+        </tr>
+        <tr>
+            
+            <td colspan="1" class="v-align-top text-center fs-7"><i>(фамилия, имя, отчество, законного представителя обучающегося, абитуриента)</i></td>
+        </tr>
+    </table>
+    <table width="100%">
+        <tr>
+            <td class="bb h-20"></td>
             <td class="text-center"> года рождения, паспорт/иной документ удостоверяющий личность серия</td>
-            <td class="bb" width="5%"><?= $passport['series'] ?></td>
+            <td class="bb h-20" width="5%"></td>
             <td width="7%">номер</td>
-            <td class="bb" width="11%"><?= $passport['number'] ?></td>
+            <td class="bb h-20" width="11%"></td>
         </tr>
     </table>
     <table width="100%">
         <tr>
             <td width="17%"> кем и когда выдан</td>
-            <td width="83%" colspan="4" class="bb text-center"><?= $passport['authority'] ?> <?= $passport['date_of_issue'] ?></td>
+            <td width="83%" colspan="4" class="bb text-center h-20"></td>
         </tr>
     </table>
 <?php if ($cpk) : ?>
     <table width="100%">
         <tr>
             <td width="32%">проживающий(ая) по адресу:</td>
-            <td width="68%" colspan="4" class="bb text-center"><?= $reg['full'] ?></td>
+            <td width="68%" colspan="4" class="bb text-center h-20"></td>
         </tr>
     </table>
 
@@ -67,110 +67,11 @@ if ($cpk) {
     <table width="100%">
         <tr>
             <td width="20%">телефон:</td>
-            <td width="30%" colspan="4" class="bb text-center"><?= $profile['phone'] ?></td>
+            <td width="30%" colspan="4" class="bb text-center"></td>
             <td width="5%"></td>
             <td width="15%">e-mail:</td>
-            <td width="30%" colspan="4" class="bb text-center"><?= $profile['email']?></td>
-        </tr>
-    </table>
-    <!-- <table width="50%">
-        <tr>
-            
-        </tr>
-    </table> -->
-
-
-<?php if ($passport['age'] < 18): ?>
-    <table class="mt-30" width="100%">
-        <tr>
-            <td width="10px">Я,</td>
-            <td class="bb"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td class="text-center fs-7 h-30"><i>(фамилия, имя, отчество, дата рождения законного представителя)</i>
-            </td>
-        </tr>
-    </table>
-    <table width="100%">
-        <tr>
-            <td width="86%" class="bb h-30"></td>
-            <td width="14%">года рождения,</td>
+            <td width="30%" colspan="4" class="bb text-center"></td>
         </tr>
     </table>
 
-    <table width="100%">
-        <tr>
-            <td width="50%" class="h-30">паспорт/иной документ удостоверяющий личность серия</td>
-            <td class="bb"></td>
-            <td width="7%">номер</td>
-            <td class="bb" width="15%"></td>
-            <td width="19%">, кем и когда выдан</td>
-            <td class="bb"></td>
-        </tr>
-        <tr>
-            <td colspan="6" class="bb h-30"></td>
-        </tr>
-    </table>
 
-    <table width="100%">
-        <tr>
-            <td class="h-30" width="32%">зарегистрированный(ая) по адресу:</td>
-            <td width="68%" class="bb"></td>
-        </tr>
-        <tr>
-            <td colspan="2" class="bb h-30"></td>
-        </tr>
-    </table>
-
-    <table width="100%">
-        <tr>
-            <td width="32%" class="h-30">являясь законным представителем</td>
-            <td class="bb"></td>
-        </tr>
-    </table>
-    <table width="100%">
-        <tr>
-            <td class="bb h-30" width="35%"></td>
-            <td width="17%">(далее – Субъект)</td>
-            <td class="bb" width="34%"></td>
-            <td>года рождения,</td>
-        </tr>
-    </table>
-    <table width="100%">
-        <tr>
-            <td width="50%" class="h-30">паспорт/иной документ удостоверяющий личность серия</td>
-            <td class="bb"></td>
-            <td width="7%">номер</td>
-            <td class="bb" width="15%"></td>
-            <td width="19%">, кем и когда выдан</td>
-            <td class="bb"></td>
-        </tr>
-        <tr>
-            <td colspan="6" class="bb h-30"></td>
-        </tr>
-    </table>
-
-    <table width="100%">
-        <tr>
-            <td class="h-30" width="32%">зарегистрированного(ой) по адресу:</td>
-            <td width="68%" class="bb"></td>
-        </tr>
-        <tr>
-            <td colspan="2" class="bb h-30"></td>
-        </tr>
-    </table>
-
-    <table width="100%">
-        <tr>
-            <td width="14%" class="h-30">на основании</td>
-            <td class="bb"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td class="fs-7 text-center"><i>(документ, подтверждающий, что Субъект является законным представителем
-                    поступающего)</i>
-            </td>
-        </tr>
-    </table>
-<?php endif; ?>
