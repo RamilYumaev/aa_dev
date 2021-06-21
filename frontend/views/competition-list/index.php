2bde3855e9b29bc1213b671cc4f4fcfe5f767d25
Anastasia <anastasiarebr@gmail.com>
2021-06-21 18:45:31 +0300

    Update index.php

--- frontend/views/competition-list/index.php
+++ frontend/views/competition-list/index.php
@@ -10,15 +10,16 @@ $this->title = "Конкурсные списки";
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
+
 <div>
-    <h1><?= $this->title ?></h1>
-    <div>
+    <h1 style="margin-top: 54px; text-align: center"><?= $this->title ?></h1>
+    <div style="margin: 0 auto; margin-top: 30px; width: 580px; ">
         <?php foreach (\modules\dictionary\models\CompetitionList::listTitle() as $value): ?>
-        <h2><?= Html::a($value['name'], [$value['url']]) ?></h2>
+        <h3 style="text-size: 25px; font-weight: 100" ><?= Html::a($value['name'], [$value['url']]) ?></h2>
         <?php endforeach; ?>
         <?php foreach (\modules\dictionary\models\CompetitionList::listTitle(true)  as $key => $value): ?>
             <?php if($key != \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_BACHELOR) continue ?>
-            <h2><?= Html::a($value['name'], [$value['url']]) ?></h2>
+            <h3><?= Html::a($value['name'], [$value['url']]) ?></h2>
         <?php endforeach; ?>
     </div>
 </div>
