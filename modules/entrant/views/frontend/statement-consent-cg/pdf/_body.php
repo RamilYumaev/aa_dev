210d64b4b3ce3fd499e18dff059fbbb335d4693e
Ramil <ramilka06@inbox.ru>
2021-06-22 14:20:38 +0300

    fix

--- modules/entrant/views/frontend/statement-consent-cg/pdf/_body.php
+++ modules/entrant/views/frontend/statement-consent-cg/pdf/_body.php
@@ -14,7 +14,7 @@
 
 
 /* @var $statementConsent modules\entrant\models\StatementConsentCg */
-$nameFull = $profile['last_name'] . " " . $profile['first_name'] . " ".$profile['patronymic'];
+$nameFull = $profile['last_name'] . " " . $profile['first_name'] . " " . $profile['patronymic'];
 
 ?>
 <div class="fs-15" style="margin-top: 100px">
@@ -22,70 +22,79 @@ $nameFull = $profile['last_name'] . " " . $profile['first_name'] . " ".$profile[
 
 
     <p align="justify" class="lh-1-5">
-        Я, <?=$nameFull?>, <?= $passport['date_of_birth'] ?> года рождения
+        Я, <?= $nameFull ?>, <?= $passport['date_of_birth'] ?> года рождения
         <?= $profile['gender'] == "мужской" ? "согласен" : "согласна" ?>
         на зачисление на 1 курс в <?= $cg['faculty'] ?>.
     </p>
 
-    <?php $educationDocument = "серия: ".$education['series']." номер: ".$education['number'].", выданный: "
-        .$education['school_id'].", год выдачи: ".$education['year']?>
+    <?php $educationDocument = "серия: " . $education['series'] . " номер: " . $education['number'] . ", выданный: "
+        . $education['school_id'] . ", год выдачи: " . $education['year'] ?>
 
     <p><strong>Направление подготовки:</strong> <?= $cg['specialty'] ?>.</p>
     <?php
-    if ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_BACHELOR)
-    {
-        $specialization = "<p><strong>Профиль бакалавриата: </strong>" . $cg['specialization'].".";
-    }elseif ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_MAGISTER)
-    {
-        $specialization = "<p><strong>Магистерская программа: </strong>" . $cg['specialization'].".";
-    }
-    elseif ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_GRADUATE_SCHOOL)
-    {
-        $specialization = "<p><strong>Основная профессиональная образовательная программа: </strong>" . $cg['specialization'].".";
-    }else{
+    if ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_BACHELOR) {
+        $specialization = "<p><strong>Профиль бакалавриата: </strong>" . $cg['specialization'] . ".";
+    } elseif ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_MAGISTER) {
+        $specialization = "<p><strong>Магистерская программа: </strong>" . $cg['specialization'] . ".";
+    } elseif ($cg['education_level'] == \dictionary\helpers\DictCompetitiveGroupHelper::EDUCATION_LEVEL_GRADUATE_SCHOOL) {
+        $specialization = "<p><strong>Основная профессиональная образовательная программа: </strong>" . $cg['specialization'] . ".";
+    } else {
         $specialization = "";
     }
     ?>
 
-    <?=$specialization?>
-    <p><strong>Основание приема:</strong> <?=$cg['special_right']?>.
+    <?= $specialization ?>
+    <p><strong>Основание приема:</strong> <?= $cg['special_right'] ?>.
 
     <p><strong>Форма обучения:</strong> <?= $cg['edu_form'] ?>.</p>
 
     <p><strong>Вид финансирования:</strong> <?= $cg['financing_type_id'] ?>.</p>
-        <?php
-        $is086 = "";
-        if($cg['is086']){
-            $is086 = $cg['financing_type_id'] == "Бюджет" || $cg['foreigner_status'] ? " и медицинскую справку по форме 086/у" :" и предоставления медицинской
+    <?php
+    $is086 = "";
+    if ($cg['is086']) {
+        $is086 = $cg['financing_type_id'] == "Бюджет" || $cg['foreigner_status'] ? " и медицинскую справку по форме 086/у" : " и предоставления медицинской
 справки по форме 086/у";
-        }?>
-Подтверждаю, что у меня отсутствуют действительные (не отозванные) заявления о согласии на зачисление на обучение по программам высшего образования данного уровня на места в рамках контрольных цифр приема, в том числе, поданные в другие организации.
+    } ?>
+    Подтверждаю, что у меня отсутствуют действительные (не отозванные) заявления о согласии на зачисление на обучение по
+    программам высшего образования данного уровня на места в рамках контрольных цифр приема, в том числе, поданные в
+    другие организации.
 
     <p align="justify" class="lh-1-5">
         Обязуюсь в течение первого года обучения
-        <?php if($cg['financing_type_id'] == "Бюджет"):?>
-        <?=$cg['foreigner_status'] ?
-            "предоставить в отдел по работе с иностранными учащимися оригинал и 
+        <?php if ($cg['financing_type_id'] == "Бюджет"): ?>
+            <?= $cg['foreigner_status'] ?
+                "предоставить в отдел по работе с иностранными учащимися оригинал и 
             нотариально заверенный перевод документа об образовании $educationDocument$is086." :
-        "предоставить в МПГУ оригинал документа об образовании $educationDocument$is086, удостоверяющего образование соответствующего уровня,
-        необходимого для зачисления на места за счет бюджетных ассигнований федерального бюджета."?>
-        <?php else :?>
-            <?=$cg['foreigner_status'] ?
+                "предоставить в МПГУ оригинал документа об образовании $educationDocument$is086, удостоверяющего образование соответствующего уровня,
+        необходимого для зачисления на места за счет бюджетных ассигнований федерального бюджета." ?>
+        <?php else : ?>
+            <?= $cg['foreigner_status'] ?
                 "предоставить в отдел по работе с иностранными учащимися, 
             нотариально заверенный перевод документа об образовании $educationDocument$is086." :
                 "явиться (вместе с Заказчиком)
-                в приемную комиссию МПГУ для личного подписания оригинала Договора$is086."?>
-        <?php endif;?>
+                в приемную комиссию МПГУ для личного подписания оригинала Договора$is086." ?>
+        <?php endif; ?>
     </p>
-    Обязуюсь в течение первого года обучения пройти обязательные предварительные медицинские осмотры (обследования) при обучении по специальностям и направлениям подготовки, входящим в перечень специальностей и направлений подготовки, при приеме на обучение по которым поступающие проходят обязательные предварительные медицинские осмотры (обследования), в порядке, установленном при заключении трудового договора или служебного контракта по соответствующей должности или специальности, утвержденном постановлением Правительства Российской Федерации от 14 августа 2013 г. № 697.
-
+    <?php if ($cg['is086']): ?>
+        <p align="justify">
+            Обязуюсь в течение первого года обучения пройти обязательные предварительные медицинские осмотры
+            (обследования)
+            при обучении по специальностям и направлениям подготовки, входящим в перечень специальностей и направлений
+            подготовки,
+            при приеме на обучение по которым поступающие проходят обязательные предварительные медицинские осмотры
+            (обследования),
+            в порядке, установленном при заключении трудового договора или служебного контракта по соответствующей
+            должности или специальности, утвержденном постановлением Правительства Российской Федерации от 14 августа
+            2013 г. № 697.
+        </p>
+    <?php endif; ?>
     <table width="100%" class="mt-50 fs-15">
         <tr>
             <td width="25%"></td>
-            <td width="15%"><?=date("d.m.Y")?> г.</td>
+            <td width="15%"><?= date("d.m.Y") ?> г.</td>
             <td class="bb" width="35%"></td>
             <td width="25%"><?= $nameFull ?></td>
-            
+
         </tr>
         <tr>
             <td width="25%"></td>
@@ -93,7 +102,7 @@ $nameFull = $profile['last_name'] . " " . $profile['first_name'] . " ".$profile[
             <td width="35%" class="v-align-top text-center">(подпись поступающего)</td>
             <td width="15%"></td>
             <td width="25%"></td>
-            
+
         </tr>
     </table>
 </div>
