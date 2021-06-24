2e306d0f87d247fbbcda8431a799416724e8ed57
Ramil <ramilka06@inbox.ru>
2021-06-25 00:58:10 +0300

    fix

--- frontend/views/switch-user/user.php
+++ frontend/views/switch-user/user.php
@@ -3,6 +3,8 @@
 use modules\entrant\helpers\BlockRedGreenHelper;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use xj\qrcode\QRcode;
+use xj\qrcode\widgets\Text;
 /* @var $this yii\web\View */
 /* @var $user common\auth\models\User*/
 /* @var $model frontend\search\Profile*/
@@ -22,6 +24,14 @@ use yii\widgets\DetailView;
             'status:boolean'
         ]
     ]) ?>
+        <?= Text::widget([
+            'outputDir' => '@webroot/qr',
+            'outputDirWeb' => '@web/qr',
+            'ecLevel' => QRcode::QR_ECLEVEL_L,
+            'text' => 'https://sdo.mpgu.org/switch-user/by-user-id?id=' . $user->id,
+            'size' => 8,
+        ]);
+        ?>
     <?php else: ?>
         <p>Ничего не найдено</p>
     <?php endif; ?>
