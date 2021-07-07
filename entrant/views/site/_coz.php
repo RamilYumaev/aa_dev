4e5e97f95735ee6474bb11532919ca0511bc0972
Ramil <ramilka06@inbox.ru>
2021-07-07 13:24:51 +0300

    fix_coz_lk

--- entrant/views/site/_coz.php
+++ entrant/views/site/_coz.php
@@ -3,6 +3,7 @@
 
 use modules\entrant\helpers\AisReturnDataHelper;
 use modules\entrant\widgets\cpk\CountPotentialUserCozWidget;
+use modules\entrant\widgets\cpk\CountUserCozFokWidget;
 use modules\entrant\widgets\cpk\CountUserCozWidget;
 use backend\widgets\adminlte\components\AdminLTE;
 use modules\entrant\widgets\cpk\CountUserErrorFilesCozWidget;
@@ -11,20 +12,28 @@ use modules\entrant\widgets\file\FileWidget;
 
 ?>
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-4">
         <?= CountUserCozWidget::widget([
             'colorBox' => AdminLTE::BG_YELLOW_ACTIVE,
             'entrant' => $jobEntrant,
             'icon' => 'user',
             'str' => "Абитуриенты (всего)", 'link' => ['data-entrant/default/index']]) ?>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-4">
         <?= CountUserCozWidget::widget([
             'type' => 1,
             'colorBox' => AdminLTE::BG_GREEN_ACTIVE,
             'entrant' => $jobEntrant,
             'icon' => 'user-plus',
             'str' => "Абитуриенты  (Принятые)", 'link' => ['data-entrant/default/index', 'type' => AisReturnDataHelper::AIS_YES]]) ?>
+    </div>
+    <div class="col-md-4">
+        <?= CountUserCozFokWidget::widget([
+            'type' => 3,
+            'colorBox' => AdminLTE::BG_AQUA,
+            'entrant' => $jobEntrant,
+            'icon' => 'user',
+            'str' => "Абитуриенты (Взяты в работу)", 'link' => ['data-entrant/default/index', 'type' => 3]]) ?>
     </div>
 </div>
 <div class="row">
@@ -66,4 +75,3 @@ use modules\entrant\widgets\file\FileWidget;
     </div>
 
 </div>
-
