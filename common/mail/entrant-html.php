2c213af93c28c860f207160f17a21b5c22695776
Ramil Yumaev <ra.yumaev@mpgu.su>
2022-09-03 12:20:40 +0300

    Fix exam letter text

--- common/mail/entrant-html.php
+++ common/mail/entrant-html.php
@@ -14,7 +14,7 @@ use yii\helpers\Html; ?>
     <?= $text ?>: <?= Html::a(Html::encode($url), $url) ?>
     ,предварительно авторизовавшись в личном кабинете поступающего в МПГУ. <br />
     <?php if($time) : ?>
-        Начать экзамен рекомендуем в <?= $time?> по МСК. Предварительно необходимо пройти процедуру индентификации и допуска к экзамену. Начать идентификацию рекомендуем за полчаса до начала экзамена.  <br />
+        Начать экзамен рекомендуем в <?= $time?> по МСК. До начала экзамена необходимо пройти процедуру идентификации и допуска к экзамену. <br />
     Подробнее о прохождении дистанционного вступительного экзамена Вы можете узнать по ссылке на инструкцию : <?= Html::a(Html::encode('https://docs.google.com/document/d/1tG61PxT5x4ku9deJtYbZPCoJu6VF9xtuJ5xd9wIWLGo/edit'), 'https://docs.google.com/document/d/1tG61PxT5x4ku9deJtYbZPCoJu6VF9xtuJ5xd9wIWLGo/edit') ?>
         или просмотрев видео-инструкцию  <?= Html::a(Html::encode('https://www.youtube.com/watch?v=NUelbilye5Q&feature=youtu.be'), 'https://www.youtube.com/watch?v=NUelbilye5Q&feature=youtu.be') ?> <br />
 
@@ -22,7 +22,7 @@ use yii\helpers\Html; ?>
 
         Техническая поддержка: <br />
         support_priem@mpgu.edu <br />
-        группы телеграмм: <br />
+        группы telegram, где можно согласовать другое время экзамена, а также проконсультироваться по различным вопросам: <br />
         Для поступающих на программы бакалавриата: <?= Html::a(Html::encode('https://t.me/+ltXhe1C6PUE4N2Iy'),'https://t.me/+ltXhe1C6PUE4N2Iy' ) ?><br />
         Для поступающих в магистратуру: <?= Html::a(Html::encode('https://t.me/+IeHReBSlLgtlOWQy'),'https://t.me/+IeHReBSlLgtlOWQy' ) ?> <br />
         Для поступающих в аспирантуру: <?= Html::a(Html::encode('https://t.me/+pcmmF8Lm-FE3MjQy'),'https://t.me/+pcmmF8Lm-FE3MjQy' ) ?><br />
