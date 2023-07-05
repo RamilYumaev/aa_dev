7a495aaabb8d3d58355c7f7fb71e7d6248ad6df5
Mark <markpdd13@list.ru>
2023-07-05 12:23:02 +0300

    Fix

--- common/moderation/widgets/views/export/index.php
+++ common/moderation/widgets/views/export/index.php
@@ -5,8 +5,8 @@
 ?>
 <div class="box box-danger">
     <?php if($model): ?>
-    <div class="pull-right">
         <div class="box-body" style="margin: 10px">
+            <div class="pull-right">
             <?= \yii\helpers\Html::a('Обновить данные в АИС ВУЗ', ['update-export-data',  'id' => $id,'did' => $model->id], ['class'=> 'btn btn-success', 'data' => [
                     'confirm' => 'Вы уверены, что хотите обновить данные в АИС ВУЗ?',
                     'method' => 'post',
