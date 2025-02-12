b25d12557455862e4cd735bbc7f575a2752671f3
markpdd13 <markpdd13@list.ru>
2025-02-12 15:04:26 +0300

    Add button main page

--- frontend/views/site/anti-ddos.php
+++ frontend/views/site/anti-ddos.php
@@ -1,5 +1,6 @@
 <div class="row">
     <div class="col-md-offset-4 col-md-4 mt-60">
-        Для продолжения, пожалуйста, нажмите на кнопку <a href="/site/anti-ddos" class="btn btn-danger" data-method="post">Войти</a>
+        Для продолжения, пожалуйста, нажмите на кнопку
+        <a href="/site/anti-ddos" class="btn btn-danger" data-method="post">Далее</a>
     </div>
 </div>
